e1508a4b1d70ea4337829c66d4ae90c98eb6652d
9731068 <hh_1927@aut.ac.ir>
2019-06-04 18:42:48 +0430

    int Search class setLableProperties method created and painted the lable

--- src/Graphics/north/Search.java
+++ src/Graphics/north/Search.java
@@ -18,4 +18,14 @@ public class Search extends JPanel {
         this.add(searchIcon, BorderLayout.WEST);
 
     }
+
+    public JLabel setLableProperties(JLabel label){
+        label.setOpaque(true);
+        label.setBackground(new Color(3, 11, 21  ));
+        label.setForeground(Color.WHITE);
+        ImageIcon icon = new ImageIcon("C:\\Users\\hashemi\\Desktop\\Jpotify\\src\\Graphics\\icons\\wicon\\26.png");
+        Image newimg = icon.getImage().getScaledInstance( 20, 20,  java.awt.Image.SCALE_SMOOTH ) ;
+        label.setIcon(new ImageIcon( newimg ));
+        return label;
+    }
 }
