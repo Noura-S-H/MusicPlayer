f82c047e2e97254a30da98f5fa2c835296b2c064
9731068 <hh_1927@aut.ac.ir>
2019-06-04 18:26:01 +0430

    In the graphic file in the east section FriendActivity.java created

---
+++ src/Graphics/east/FriendActivity.java
@@ -0,0 +1,18 @@
+package Graphics;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FriendActivity extends JPanel {
+
+
+    public FriendActivity(){
+        super();
+        this.setLayout(new BorderLayout());
+        //this.add(,BorderLayout.CENTER);
+        this.setBackground(new Color(31,37,47));
+        this.setVisible(true);
+    }
+
+
+}
