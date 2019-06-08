afb02339e4dca8be12ae9c8cdf49e529ebb9aba8
9731068 <hh_1927@aut.ac.ir>
2019-06-08 10:48:19 +0430

    setLableIcon method and new lable for sound add to PlaySetting

--- src/Graphics/south/center/PlaySetting.java
+++ src/Graphics/south/center/PlaySetting.java
@@ -5,27 +5,31 @@ import java.awt.*;
 
 public class PlaySetting extends JPanel {
 
-    JLabel l = new JLabel();
+    JLabel emptySpace = new JLabel();
+    JLabel sound = new JLabel();
+
     public PlaySetting(){
         super();
         this.setLayout(new BorderLayout());
 
-        setLableProperties(l);
-        this.add(l,BorderLayout.CENTER);
+        setLableProperties(emptySpace);
+        this.add(emptySpace,BorderLayout.CENTER);
         this.setVisible(true);
     }
 
    // 😍 😃
 
-    public JLabel setLableProperties(JLabel label){
+    public void setLableProperties(JLabel label){
         label.setOpaque(true);
         label.setBackground(new Color(3, 11, 21  ));
         label.setForeground(Color.WHITE);
         label.setPreferredSize(new Dimension(100,30));
+    }
+
+    public void setLableIcon(JLabel label){
         ImageIcon icon = new ImageIcon("C:\\Users\\hashemi\\Desktop\\Jpotify\\src\\Graphics\\icons\\wicon\\33.png");
         Image newimg = icon.getImage().getScaledInstance( 20, 20,  java.awt.Image.SCALE_SMOOTH ) ;
         label.setIcon(new ImageIcon( newimg ));
-        return label;
     }
 
 }
