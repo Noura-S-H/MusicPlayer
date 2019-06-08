fe04b98d93281a10e6a769bc0bb69ebefd6468fc
9731068 <hh_1927@aut.ac.ir>
2019-06-08 13:00:46 +0430

    search icon changed

--- src/Graphics/north/Search.java
+++ src/Graphics/north/Search.java
@@ -5,17 +5,21 @@ import java.awt.*;
 
 public class Search extends JPanel {
 
+
     private final JLabel searchIcon;
+    private JTextField searchBar;
 
     public Search() {
         super();
-        this.setLayout(new BorderLayout());
+        this.setLayout(new FlowLayout());
         this.setBackground(new Color(3, 11, 21));
         this.setVisible(true);
-
+        searchBar = new JTextField();
         searchIcon = new JLabel();
+
         setLableProperties(searchIcon);
-        this.add(searchIcon, BorderLayout.WEST);
+        this.add(searchIcon);
+        this.add(searchBar);
 
     }
 
@@ -23,9 +27,11 @@ public class Search extends JPanel {
         label.setOpaque(true);
         label.setBackground(new Color(3, 11, 21  ));
         label.setForeground(Color.WHITE);
-        ImageIcon icon = new ImageIcon("C:\\Users\\hashemi\\Desktop\\Jpotify\\src\\Graphics\\icons\\wicon\\26.png");
+        ImageIcon icon = new ImageIcon("C:\\Users\\hashemi\\Desktop\\Jpotify\\src\\Graphics\\icons\\wicon\\search.png");
         Image newimg = icon.getImage().getScaledInstance( 20, 20,  java.awt.Image.SCALE_SMOOTH ) ;
         label.setIcon(new ImageIcon( newimg ));
         return label;
     }
+
+
 }
