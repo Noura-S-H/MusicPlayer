079221e312a2e65483c26d69a62b1eb9be232e6f
9731068 <hh_1927@aut.ac.ir>
2019-06-09 17:21:35 +0430

    create list of friends and properties

--- src/Graphics/east/FriendActivity.java
+++ src/Graphics/east/FriendActivity.java
@@ -2,16 +2,36 @@ package Graphics.east;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class FriendActivity extends JPanel {
 
+    DefaultListModel<File> playlist = new DefaultListModel<File>();
+    JList<File> list;
+    private static final String LABEL_TEXT = "  \uD83C\uDFA7  Friend Activity ";
 
     public FriendActivity(){
         super();
         this.setLayout(new BorderLayout());
-        //this.add(,BorderLayout.CENTER);
-        this.setBackground(new Color(31,37,47));
+        this.setBackground(new Color(3,11,21));
         this.setVisible(true);
+
+        JLabel friendActivity = new JLabel(LABEL_TEXT);
+        setLableProperties(friendActivity);
+        this.add(friendActivity, BorderLayout.NORTH);
+        list = new JList<File>(playlist);
+        this.add(list, BorderLayout.CENTER);
+        JScrollPane scroll = new JScrollPane(list);
+
+    }
+
+    public void setLableProperties(JLabel label){
+        label.setPreferredSize(new Dimension(100, 50));
+        label.setHorizontalAlignment(SwingConstants.LEFT);
+        label.setForeground(Color.white);
+        label.setOpaque(true);
+        label.setBackground(new Color(3, 11, 21));
+
     }
 
 
