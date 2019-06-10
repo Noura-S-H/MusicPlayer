bf1fe436c766a27e8a2c566154bbc8e170f96b62
9731068 <hh_1927@aut.ac.ir>
2019-06-10 16:46:34 +0430

    profileInfo properties and lable properties method added

--- src/Graphics/north/Profile.java
+++ src/Graphics/north/Profile.java
@@ -9,7 +9,7 @@ import java.awt.*;
 public class Profile extends JPanel {
 
     private JLabel emptySpace;
-    private String[] profile = {"name","id"};
+    private String[] profile = {"Username","id"};
     private JComboBox<String> profileInfo;
 
     public Profile(){
@@ -17,23 +17,33 @@ public class Profile extends JPanel {
         this.setLayout(new FlowLayout());
         this.setBackground(new Color(3, 11, 21));
         this.setVisible(true);
+        emptySpace = new JLabel();
         profileInfo = new JComboBox<String>(profile);
+        setLableProperties(emptySpace);
+        setJComboBoxProperties(profileInfo);
+        this.add(profileInfo);
+        this.add(emptySpace);
+    }
+
+    public void setJComboBoxProperties(JComboBox Combo){
         profileInfo.setBackground(new Color(3,11,21));
         profileInfo.setForeground(Color.white);
         profileInfo.setPreferredSize(new Dimension(150,20));
-        //profileInfo.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 12));
-
         for (int i = 0; i < profileInfo.getComponentCount(); i++) {
             if (profileInfo.getComponent(i) instanceof JComponent) {
                 ((JComponent) profileInfo.getComponent(i)).setBorder(new EmptyBorder(0, -10, 0, 0));
 
             }
         }
-
-        this.add(profileInfo);
+        //profileInfo.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 12));
     }
 
-    
+    public void setLableProperties(JLabel label){
+        label.setOpaque(true);
+        label.setPreferredSize(new Dimension(140,30));
+        label.setBackground(new Color(3, 11, 21 ));
+        label.setForeground(Color.WHITE);
+    }
 
 
 }
