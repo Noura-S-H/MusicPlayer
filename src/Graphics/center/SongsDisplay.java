6f1d08862794f89941ce9c9bdbc963d27bad480e
9731068 <hh_1927@aut.ac.ir>
2019-06-13 11:09:20 +0430

    two panel(left,right) for display song and list of songs created

--- src/Graphics/center/SongsDisplay.java
+++ src/Graphics/center/SongsDisplay.java
@@ -1,5 +1,35 @@
 package Graphics.center;
 
-public class SongsDisplay {
+import Graphics.Song;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class SongsDisplay extends JPanel {
+        private ArrayList<Song> songlist;
+        JPanel left = new JPanel();
+        JPanel right = new JPanel();
+
+        public SongsDisplay(){
+            super();
+            this.setLayout(new BorderLayout());
+            this.setBackground(new Color(3, 11, 21));
+
+            this.add(left,BorderLayout.EAST);
+            this.add(right,BorderLayout.WEST);
+
+            this.setVisible(true);
+
+        }
+
+        public void leftPanelDesigne(){
+
+        }
+
+
+        public void rightPanelDesigne(){
+
+        }
 
 }
