212374846c0e66ae88be4bb71168426c7d6e6c17
9731068 <hh_1927@aut.ac.ir>
2019-06-20 20:32:56 +0430

    all properties of JTable added to it method and removed from jpanel

--- src/Graphics/center/SongsDisplay.java
+++ src/Graphics/center/SongsDisplay.java
@@ -5,12 +5,12 @@ import Graphics.Song;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class SongsDisplay extends JPanel {
         private ArrayList<Song> songslist = new ArrayList<Song>();
-        private ArrayList<JButton> songsButtons = new ArrayList<JButton>();
 
         private JTable songsTable;
 
@@ -20,6 +20,7 @@ public class SongsDisplay extends JPanel {
         public SongsDisplay(){
             super();
             this.setLayout(new BorderLayout());
+            this.setBackground(new Color(3,11,21));
             this.setVisible(true);
 
             addsong("C:\\Users\\hashemi\\Desktop\\www.mp3");
@@ -43,31 +44,14 @@ public class SongsDisplay extends JPanel {
             list.setVisible(true);
 
             String[][] data = new String[songslist.size()][5];
-
             for(int i = 0; i < songslist.size() ; i++)
                     data[i] = songslist.get(i).getInfo();
 
-            String[] headLine = {"  \uD83D\uDD24 TITLE",
+            String[] headLine = {"   \uD83D\uDD24 TITLE",
                     "  \uD83C\uDFA4 ️ARTISTS"," \uD83D\uDCBF ALBUM"," \uD83D\uDCC6 YEAR"," \uD83D\uDD52"};
 
             songsTable = new JTable(data,headLine);
-
-            songsTable.setForeground(Color.white);
-            JTableHeader header = songsTable.getTableHeader();
-            header.setBackground(new Color(3,11,21));
-            header.setForeground(Color.WHITE);
-            header.setFont(new Font("Sherif", Font.BOLD, 12));
-
-            songsTable.setShowGrid(false);
-            ((DefaultTableCellRenderer)songsTable.getTableHeader().getDefaultRenderer())
-                    .setHorizontalAlignment(JLabel.LEFT);
-
-            songsTable.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
-            songsTable.setRowHeight(25);
-            UIManager.getDefaults().put("TableHeader.cellBorder" , BorderFactory.createEmptyBorder(0,0,0,0));
-            songsTable.setPreferredSize(new Dimension(100,200));
-            songsTable.setBackground(new Color(3,11,21));
-
+            setTableProperties(songsTable);
             list.add(new JScrollPane(songsTable),BorderLayout.CENTER);
 
         }
@@ -92,47 +76,68 @@ public class SongsDisplay extends JPanel {
         }
 
         public void sortSongByTime(){
-
         }
 
-    public void setButtonsProperties(JButton button,int w,int h){
+        public void setButtonsProperties(JButton button,int w,int h){
 
-        button.setBorder(BorderFactory.createEmptyBorder());
-        button.setBackground(new Color(3, 11, 21));
-        button.setOpaque(true);
-        button.setPreferredSize(new Dimension(w,h));
-     //   button.setFont(new Font("Brush Script MT", Font.PLAIN, 15));
-        button.setHorizontalAlignment(SwingConstants.LEFT);
-        button.setForeground(Color.white);
-    }
-
-    public  void setLableProperties(JLabel label){
-        label.setOpaque(true);
-        label.setBackground(new Color(3, 11, 21 ));
-        label.setPreferredSize(new Dimension(600,110));
-        label.setFont(new Font("Brush Script MT", Font.PLAIN, 14));
-        label.setForeground(Color.WHITE);
-    }
-
-
-    public ImageIcon getImage(String path,int w, int h){
-        ImageIcon imIcon = new ImageIcon(path);
-        Image newimg = imIcon.getImage().getScaledInstance( w, h,  java.awt.Image.SCALE_SMOOTH ) ;
-        imIcon = new ImageIcon( newimg );
-        return imIcon;
-    }
-
-    public void addsong(String path){
-            Song s = new Song(path);
-            songslist.add(s);
-    }
-
-    public void addSongInfoToSongButton(){
-        for(int i = 0; i<songslist.size(); i++){
-            songsButtons.add(new JButton());
-     //       songsButtons.get(i).setText(songslist.get(i).getInfo());
+            button.setBorder(BorderFactory.createEmptyBorder());
+            button.setBackground(new Color(3, 11, 21));
+            button.setOpaque(true);
+            button.setPreferredSize(new Dimension(w,h));
+     //     button.setFont(new Font("Brush Script MT", Font.PLAIN, 15));
+            button.setHorizontalAlignment(SwingConstants.LEFT);
+            button.setForeground(Color.white);
         }
-    }
+
+        public  void setLableProperties(JLabel label){
+            label.setOpaque(true);
+            label.setBackground(new Color(3, 11, 21 ));
+            label.setPreferredSize(new Dimension(600,110));
+            label.setFont(new Font("Brush Script MT", Font.PLAIN, 14));
+            label.setForeground(Color.WHITE);
+        }
+
+        public void setTableProperties(JTable tb){
+
+            JTableHeader header = tb.getTableHeader();
+            header.setBackground(new Color(3,11,21));
+            header.setForeground(Color.WHITE);
+            header.setFont(new Font("Sherif", Font.BOLD, 12));
+            UIManager.getDefaults().put("TableHeader.cellBorder" , BorderFactory.createEmptyBorder(0,0,0,0));
+
+            tb.setShowGrid(false);
+            ((DefaultTableCellRenderer)tb.getTableHeader().getDefaultRenderer())
+                    .setHorizontalAlignment(JLabel.LEFT);
+            tb.setRowHeight(25);
+            tb.setForeground(Color.white);
+            tb.setBackground(new Color(3,11,21));
+            tb.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
+            songsTable.setPreferredSize(new Dimension(100,200));
+
+            TableColumn column = null;
+            for(int i = 0; i < 5; i++ ){
+                column = tb.getColumnModel().getColumn(i);
+                if (i == 0)
+                    column.setPreferredWidth(250); //sport column is bigger
+                else if(i == 1 || i ==2 )
+                    column.setPreferredWidth(150);
+                else
+                    column.setPreferredWidth(50);
+            }
+        }
+
+        public ImageIcon getImage(String path,int w, int h){
+            ImageIcon imIcon = new ImageIcon(path);
+            Image newimg = imIcon.getImage().getScaledInstance( w, h,  java.awt.Image.SCALE_SMOOTH ) ;
+            imIcon = new ImageIcon( newimg );
+            return imIcon;
+        }
+
+        public void addsong(String path){
+                Song s = new Song(path);
+                songslist.add(s);
+        }
+
 
 
 
