ff62b539239cba21d16b3a3184d23cfd737c540d
9731068 <hh_1927@aut.ac.ir>
2019-06-23 00:22:21 +0430

    check file is empty then appanded that

--- src/Graphics/west/AddToLibrary.java
+++ src/Graphics/west/AddToLibrary.java
@@ -12,6 +12,7 @@ public class AddToLibrary extends Component implements ActionListener {
     private ActionEvent e;
     private JButton  button;
     ArrayList<String> filePath;
+    private final String path = "src\\Files\\Songs.txt";
 
     public AddToLibrary(JButton button) {
         this.button = button;
@@ -37,21 +38,26 @@ public class AddToLibrary extends Component implements ActionListener {
                 System.out.println(ex.toString());
 
             }
-            addSongstoLibrary("src\\Files\\output.txt");
+            addSongstoLibrary(path);
         }
     }
 
 
     public void addSongstoLibrary(String pt){
-
+        boolean empty = false;
         File f = new File(pt);
-        try {
-            if (!f.exists()) {
-                f.createNewFile();
-            }
-            PrintWriter file = null;
+        PrintWriter file = null;
 
-            file = new PrintWriter(new FileWriter(f));
+        if(f.length() == 0)
+            empty = true;
+        try {
+            if (!f.exists())
+                f.createNewFile();
+
+            file = new PrintWriter(new FileWriter(f,true));
+            if(empty == false)
+                file.println();
+
             for(int i = 0; i < filePath.size() ;i++) {
                 if(i == filePath.size()-1)
                     file.print(filePath.get(i));
