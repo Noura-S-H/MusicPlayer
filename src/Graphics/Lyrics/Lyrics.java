dbfa779aaf9b1f1fe4e4e814da58bfa203122708
9731068 <hh_1927@aut.ac.ir>
2019-06-25 23:18:20 +0430

    lyrics cladd with libraryes added  and additional file deleted

---
+++ src/Graphics/Lyrics/Lyrics.java
@@ -0,0 +1,40 @@
+package Graphics.Lyrics;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lyrics {
+
+
+    private final static String songLyricsURL = "http://www.songlyrics.com";
+
+
+    public static List<String> getSongLyrics(String band, String songTitle) throws IOException {
+        List<String> lyrics= new ArrayList<String>();
+
+        Document doc = Jsoup.connect(songLyricsURL+ "/"+band.replace(" ", "-").toLowerCase()+
+                "/"+songTitle.replace(" ", "-").toLowerCase()+"-lyrics/").userAgent("Mozilla").get();
+        String title = doc.title();
+        System.out.println(title);
+        Element p = doc.select("p.songLyricsV14").get(0);
+        for (Node e: p.childNodes()) {
+            if (e instanceof TextNode) {
+                lyrics.add(((TextNode)e).getWholeText());
+            }
+        }
+        return lyrics;
+    }
+
+//    public static void main(String[] args) throws IOException {
+//        System.out.println(Lyrics.getSongLyrics("U2", "With or Without You"));
+//        System.out.println(Lyrics.getSongLyrics("Billy Joel", "Allentown"));
+//        System.out.println(Lyrics.getSongLyrics("Tori Amos", "Winter"));
+//    }
+}
